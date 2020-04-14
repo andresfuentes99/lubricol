fc3206a35340a9589492dec96fd2a9d40846a78a
andresfuentes99 <63633735+andresfuentes99@users.noreply.github.com>
2020-04-14 01:48:52 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+var img = document.getElementById("imagen");
+var uno = document.getElementById("uno");
+var dos = document.getElementById("dos");
+var tres = document.getElementById("tres");
+var icuatro = document.getElementById("cuatro");
+
+uno.onclick = function(){
+    img.src = "/img/banner.jpg"
+    document.getElementById("card");
+}
+dos.onclick = function(){
+    img.src = "/img/banner-2.jpg"
+    document.getElementById("card");
+}
+tres.onclick = function(){
+    img.src = "/img/banner-3.jpg"
+    document.getElementById("card");
+}
+curo.onclick = function(){
+    img.src = "/img/banner-4.jpg"
+    document.getElementById("card");
+}
